52c21a51a6bff2065a90a554b27b5c95d5cc87fd
wooddang-gachon <al457616@gachon.ac.kr>
2026-07-03 20:08:16 +0900

    Initial commit

---
+++ src/test/java/com/gdgoc/monitor/chat_service/ChatServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdgoc.monitor.chat_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
