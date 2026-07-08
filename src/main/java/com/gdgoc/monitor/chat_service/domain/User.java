8ed88139efd7724076827f587ef12b53ea8831d0
wooddang-gachon <al457616@gachon.ac.kr>
2026-07-08 15:07:11 +0900

    Feat: domain 추가

---
+++ src/main/java/com/gdgoc/monitor/chat_service/domain/User.java
@@ -0,0 +1,29 @@
+package com.gdgoc.monitor.chat_service.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String identify;
+
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(nullable = false, length = 50)
+    private String nickname;
+}
